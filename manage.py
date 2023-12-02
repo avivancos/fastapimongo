979cf6d99c7a87472fe6a5397b7adf87185f67a4
Agustin <agusvc@gmail.com>
2023-12-02 10:33:39 +0100

    Add app initialization and version creation commands

--- manage.py
+++ manage.py
@@ -3,14 +3,40 @@ import os
 import subprocess
 from multiprocessing import Process
 from time import sleep
-
 import click
 import requests
+
 
 @click.command()
 def shell():
     """Starts the python shell."""
     subprocess.run(["python"])
+
+#app initialization command
+@click.command(short_help="Initializes the application.")
+def init():
+    """Initializes the application."""
+    if os.path.exists("main.py"):
+        click.echo("The application is already initialized.")
+        return
+    with open("main.py", "w") as f:
+        f.write("""from fastapi import FastAPI""")
+    click.echo("Application initialized.")
+    
+#CREATE a new version app directory
+
+@click.command(short_help="Creates a new version app directory.")
+@click.argument('versionnumber ')
+def version(version_number):
+    """Creates a new  # version app directory."""
+    os.makedirs(f'v{version_number}/models', exist_ok=True)
+    os.makedirs(f'v{version_number}/schemas', exist_ok=True)
+    os.makedirs(f'v{version_number}/routers', exist_ok=True)
+    os.makedirs(f'v{version_number}/services', exist_ok=True)
+
+    with open(f'v{version_number}/main.py', 'w') as f:
+        f.write("""from fastapi import FastAPI""")
+    click.echo(f'Version {version_number} created with standard directory structure.')
 
 @click.command(short_help="Creates a new superuser.")
 def createsuperuser():
@@ -71,50 +97,49 @@ def cli():
 @cli.command()
 @click.pass_context
 def help(ctx):
-    """Shows help about the available commands"""
+    """
+    Shows help about the available commands
+
+    Parameters:
+    - ctx: The click context object
+
+    Returns:
+    None
+    """
     click.echo("Available commands:")
     click.echo()
     for cmd_name in sorted(cli.commands.keys()):
         cmd = cli.commands[cmd_name]
         click.echo(f"{cmd_name}: {cmd.short_help}")
-        
-@click.command(short_help="Creates a new project with the standard directory structure.")
-@click.argument('projectname')
-def project(projectname):
-    """Creates a new project with the standard directory structure."""
-    os.makedirs(f'{projectname}/models', exist_ok=True)
-    os.makedirs(f'{projectname}/schemas', exist_ok=True)
-    os.makedirs(f'{projectname}/routers', exist_ok=True)
-    os.makedirs(f'{projectname}/services', exist_ok=True)
-
-    with open(f'{projectname}/main.py', 'w') as f:
-        f.write("""from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get("/")
-def read_root():
-    return {"Welcome to": "FastAPI SuperMongo v:{s}"}   )""".format(s=__version__)) )
-    
-
-    click.echo(f'Project {projectname} created with standard directory structure.')
-
-cli.add_command(project)
-
+  
 @click.command(short_help="Creates a new model.")
-@click.argument('modelname')
-def createmodel(modelname):
-    """Creates a new model."""
+@click.argument('choice', type=click.Choice(['model', 'router', 'service', 'schema']))
+@click.argument('versionnumber')
+def create(choice, versionnumber):
+    """
+    Creates a new model.
+
+    Args:
+        choice (str): The name of the model to create.
+        versionnumber (int): The version number of the model.
+
+    Returns:
+        None
+    """
+    if not os.path.exists(f'v{versionnumber}'):
+        click.echo(f"Version {versionnumber} does not exist. Run version command first.")
+        return
+    
     model_content = f"""from odmantic import Model, Field
 
-class {modelname}(Model):
+class {choice}(Model):
     name: str = Field(...)
 """
-    with open(f'models/{modelname.lower()}.py', 'w') as f:
+    with open(f'v{versionnumber}/models/{choice.lower()}.py', 'w') as f:
         f.write(model_content)
-    click.echo(f'Model {modelname} created.')
+    click.echo(f'Model {choice} created over version {versionnumber}.')
 
-cli.add_command(createmodel)
+cli.add_command(create)
 
 @click.command(short_help="Runs the server.")
 def run():
@@ -122,81 +147,94 @@ def run():
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
 
-#CREATE a new version app directory
+@click.command(short_help="Creates a new module with a model, router, service, and schema directory.")
+@click.argument('module')
+def module(module):
+    """
+    Creates a new module with a model, router, service, and schema directory.
 
-@click.command(short_help="Creates a new version app directory.")
-@click.argument('versionnumber ')
-def createversion(version_number):
-    """Creates a new  # version app directory."""
-    os.makedirs(f'v{version_number}/models', exist_ok=True)
-    os.makedirs(f'v{version_number}/schemas', exist_ok=True)
-    os.makedirs(f'v{version_number}/routers', exist_ok=True)
-    os.makedirs(f'v{version_number}/services', exist_ok=True)
+    Args:
+        module (str): The name of the module.
 
-    with open(f'v{version_number}/main.py', 'w') as f:
-        f.write("""from fastapi import FastAPI""")
-    click.echo(f'Version {version_number} created with standard directory structure.')
-    
-@click.command(short_help="Creates a new module with a model, router, service, and schema.")
-@click.argument('modulename')
-def createmodule(modulename):
-    """Creates a new module with a model, router, service, and schema."""
+    """
     model_content = f"""from odmantic import Model, Field
 
-class {modulename}(Model):
+class {module}(Model):
     name: str = Field(...)
 """
-    with open(f'models/{modulename.lower()}.py', 'w') as f:
+    with open(f'models/{module.lower()}.py', 'w') as f:
         f.write(model_content)
 
     router_content = f"""from fastapi import APIRouter
+from models.{module.lower()} import {module}
+from fastapi import HTTPException
 
 router = APIRouter()
 
-@router.get("/{modulename.lower()}")
-async def get_items():
-    return []
-"""
-    with open(f'routers/{modulename.lower()}.py', 'w') as f:
-        f.write(router_content)
-
-    schema_content = f"""from pydantic import BaseModel
-
-class {modulename}(BaseModel):
-    name: str
-"""
-    with open(f'schemas/{modulename.lower()}.py', 'w') as f:
-        f.write(schema_content)
-
-    crud_content = f"""from fastapi import HTTPException
-from schemas.{modulename.lower()} import {modulename}
-
 items = []
 
-def get_item(item_id: int):
+@router.get("/{module.lower()}")
+async def get_items():
+    return items
+
+@router.post("/{module.lower()}")
+async def create_item(item: {module}):
+    items.append(item)
+    return item
+
+@router.get("/{module.lower()}/{{item_id}}")
+async def get_item(item_id: int):
     for item in items:
         if item.id == item_id:
             return item
     raise HTTPException(status_code=404, detail="Item not found")
+""" 
+    with open(f'routers/{module.lower()}.py', 'w') as f:
+        f.write(router_content)
 
-def create_item(item: {modulename}):
+    service_content = f"""from models.{module.lower()} import {module}
+
+def get_all_items():
+    return items
+
+def create_item(item: {module}):
     items.append(item)
     return item
 """
-    with open(f'services/{modulename.lower()}.py', 'w') as f:
-        f.write(crud_content)
+    with open(f'services/{module.lower()}.py', 'w') as f:
+        f.write(service_content)
 
-    click.echo(f'Module {modulename} created with model, router, service, and schema.')
+    schema_content = f"""from pydantic import BaseModel
 
-cli.add_command(project)
-cli.add_command(createmodel)        
-cli.add_command(createmodule)
-cli.add_command(shell)
-cli.add_command(createsuperuser)
-cli.add_command(test)
-cli.add_command(run)
-cli.add_command(createversion)
-cli.add_command(help)
+class {module}Base(BaseModel):
+    name: str
+
+class {module}({module}Base):
+    id: int
+
+    class Config:
+        orm_mode = True
+"""
+    os.makedirs(f'schemas/{module.lower()}', exist_ok=True)
+    with open(f'schemas/{module.lower()}/{module.lower()}.py', 'w') as f:
+        f.write(schema_content)
+
+click.echo(f'Module {module} created with standard directory structure.')
+
+
+cli.add_command(version, name="version")
+cli.add_command(create, name="create")  
+cli.add_command(module, name="module")     
+cli.add_command(check, name="check")
+cli.add_command(shell, name="shell")
+cli.add_command(createsuperuser, name="createsuperuser")
+cli.add_command(test, name="test")
+cli.add_command(run, name="run")
+cli.add_command(help, name="help")
+cli.add_command(init, name="init")
+
+
+
 
 if __name__ == '__main__':
     cli()
